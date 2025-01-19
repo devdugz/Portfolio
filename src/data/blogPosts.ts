b241dc7d3874a5ce560c5dd45ee93e27dcc667b8
realcdugz <connorduggan30@gmail.com>
2025-01-19 09:29:35 -0600

    changed order of work journal

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -23,92 +23,11 @@ export interface BlogPost {
 
 export const blogPosts: BlogPost[] = [
   {
-    title: 'Work Journal - Week 1',
-    date: 'November 2, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ1,
-    image: '/images/workjournal/WJ1.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 2',
-    date: 'November 9, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ2,
-    image: '/images/workjournal/WJ2.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 3',
-    date: 'November 16, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ3,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 4',
-    date: 'November 23, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ4,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 5',
-    date: 'November 30, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ5,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 6',
-    date: 'December 7, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ6,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 7',
-    date: 'December 14, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ7,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 8',
-    date: 'December 21, 2024',
+    title: 'Work Journal - Week 12',
+    date: 'January 18, 2025',
     preview: 'This week I...',
     description: 'This week I...', // Add description field
     content: WJ8,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 9',
-    date: 'December 28, 2024',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ9,
-    image: '/images/workjournal/WJ3.jpg',
-    tag: 'Development',
-  },
-  {
-    title: 'Work Journal - Week 10',
-    date: 'January 4, 2025',
-    preview: 'This week I...',
-    description: 'This week I...', // Add description field
-    content: WJ10,
     image: '/images/workjournal/WJ3.jpg',
     tag: 'Development',
   },
@@ -122,8 +41,26 @@ export const blogPosts: BlogPost[] = [
     tag: 'Development',
   },
   {
-    title: 'Work Journal - Week 12',
-    date: 'January 18, 2025',
+    title: 'Work Journal - Week 10',
+    date: 'January 4, 2025',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ10,
+    image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 9',
+    date: 'December 28, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ9,
+    image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 8',
+    date: 'December 21, 2024',
     preview: 'This week I...',
     description: 'This week I...', // Add description field
     content: WJ8,
@@ -131,12 +68,66 @@ export const blogPosts: BlogPost[] = [
     tag: 'Development',
   },
   {
-    title: 'Work Journal - Week 13',
-    date: 'January 25, 2025',
+    title: 'Work Journal - Week 7',
+    date: 'December 14, 2024',
     preview: 'This week I...',
     description: 'This week I...', // Add description field
-    content: WJ8,
+    content: WJ7,
     image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 6',
+    date: 'December 7, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ6,
+    image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 5',
+    date: 'November 30, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ5,
+    image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 4',
+    date: 'November 23, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ4,
+    image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 3',
+    date: 'November 16, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ3,
+    image: '/images/workjournal/WJ3.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 2',
+    date: 'November 9, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ2,
+    image: '/images/workjournal/WJ2.jpg',
+    tag: 'Development',
+  },
+  {
+    title: 'Work Journal - Week 1',
+    date: 'November 2, 2024',
+    preview: 'This week I...',
+    description: 'This week I...', // Add description field
+    content: WJ1,
+    image: '/images/workjournal/WJ1.jpg',
     tag: 'Development',
   },
 ];
