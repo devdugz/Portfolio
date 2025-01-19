b04d12f13143330c020920404268461e8086a43f
realcdugz <connorduggan30@gmail.com>
2025-01-19 10:25:25 -0600

    updated header for mobile

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,26 +2,32 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <div className='flex justify-center items-center fixed top-3 w-full z-10'>
-      <nav className='flex gap-0.2 p-0.5 border border-white/15 rounded-full backdrop-blur-md bg-white/10'>
-        <Link href='#home' className='nav-items'>
+    <div className='flex justify-center items-center fixed top-2 md:top-3 w-full z-10'>
+      <nav className='flex gap-0.5 md:gap-0.2 p-0.5 border border-white/15 rounded-full backdrop-blur-md bg-white/10 text-xs md:text-base'>
+        <Link href='#home' className='nav-items px-2 py-1 md:px-4 md:py-2'>
           Home
         </Link>
-        <Link href='#about' className='nav-items'>
+        <Link href='#about' className='nav-items px-2 py-1 md:px-4 md:py-2'>
           About
         </Link>
-        <Link href='#experience' className='nav-items'>
+        <Link
+          href='#experience'
+          className='nav-items px-2 py-1 md:px-4 md:py-2'
+        >
           Experience
         </Link>
-        <Link href='#projects' className='nav-items'>
+        <Link href='#projects' className='nav-items px-2 py-1 md:px-4 md:py-2'>
           Projects
         </Link>
-        <Link href='#workjournal' className='nav-items'>
+        <Link
+          href='#workjournal'
+          className='nav-items px-2 py-1 md:px-4 md:py-2'
+        >
           Work Journal
         </Link>
         <Link
           href='#contact'
-          className='nav-items bg-white text-gray-900 hover:bg-white-70 '
+          className='nav-items px-2 py-1 md:px-4 md:py-2 bg-white text-gray-900 hover:bg-white-70'
         >
           Contact
         </Link>
